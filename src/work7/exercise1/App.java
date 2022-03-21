158d0c19b6aaa1cf3bfafb7f9c5ecf35da2889a4
Sergei Egorov <esv89217763366@yandex.ru>
2022-03-21 12:10:06 +0300

    ver0.5

--- src/work7/exercise1/App.java
+++ src/work7/exercise1/App.java
@@ -7,32 +7,42 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         int a;
-        Manager dir = new Manager("Mr Green", 78);
-        Student[] group = new Student[10];
+        Manager dir = new Manager("Mr Green", 78); // директор
+        Teacher math = new Teacher("Odin", 55, "Math", 5); // учитель
+        Student[] group = new Student[2]; // массив учеников
         for (int i = 0; i < group.length; i += 1) {
             System.out.println(i + 1);
             if (group[i] == null) {
                 group[i] = new Student("Avgust", 25 + i, "Math", 95 + i);
-                System.out.println("Добавлен ученик"+i + " уровень: "+ group[i].getLevel());
+                System.out.println("Добавлен ученик" + i + " уровень: " + group[i].getLevel() + " :: ");
                 //System.out.println(" Klass:" + Arrays.deepToString(group));
             }
         }
+        System.out.println(Arrays.deepToString(group)); // вывод массива учеников
+        int non = 0;
+        System.out.println("hey we go again?");
+        Scanner begin = new Scanner(System.in);
+        non = begin.nextInt();
+        while (0 != non) {
+            // ожидание решения директора
+            System.out.println("Are we start a day?");
+            Scanner scanner = new Scanner(System.in);
+            boolean in = scanner.nextBoolean();
+            dir.setDay(in); // решение директора
+            if (dir.getDay() == true) {
 
-        System.out.println(Arrays.deepToString(group));
-        Scanner scanner = new Scanner(System.in);
-        boolean in = scanner.nextBoolean();
-        if (in==true){
-        Teacher math = new Teacher("Odin", 55, "Math", 5);
-        for (int i = 0; i < group.length; i += 1) {
-            group[i].addLevel(5);
-            System.out.println("Ученик"+i+  " уровень повышен до: "+ group[i].getLevel());
-        }
-        }else {
-            for (int i = 0; i < group.length; i++) {
-                System.out.println("Ученик"+i+  " уровень повышен до: "+ group[i].getLevel());
+                for (int i = 0; i < group.length; i += 1) {
+                    group[i].addLevel(5);
+                    System.out.println("Ученик" + i + " уровень повышен до: " + group[i].getLevel());
+                }
+            } else {
+                for (int i = 0; i < group.length; i++) {
+                //    System.out.println("Ученик" + i + " уровень повышен до: " + group[i].getLevel());
+                }
             }
-
+            System.out.println("Do you wont more?"); // проверка выхода из цикла
+            System.out.print("");
+            non = begin.nextInt();
         }
     }
-
 }
